ec10bcce9383438f785f32dd2c4936e289cedb70
Lucas-Coding <killeylucas@gmail.com>
2022-12-07 13:56:01 +0000

    Created Database and UserData class and filled them out

--- Models/Database.php
+++ Models/Database.php
@@ -2,5 +2,58 @@
 
 class Database
 {
+    /**
+     * @var Database
+     */
+    protected static $_dbInstance = null;
 
+    /**
+     * @var PDO
+     */
+    protected $_dbHandle;
+
+    /**
+     * @return Database
+     */
+    public static function getInstance() {
+        $username ='hc2x-2';
+        $password = 'group2';
+        $host = 'poseidon.salford.ac.uk';
+        $dbName = 'hc2x_2_marketplace';
+
+        if(self::$_dbInstance === null) { //checks if the PDO exists
+            // creates new instance if not, sending in connection info
+            self::$_dbInstance = new self($username, $password, $host, $dbName);
+        }
+
+        return self::$_dbInstance;
+    }
+
+    /**
+     * @param $username
+     * @param $password
+     * @param $host
+     * @param $database
+     */
+    private function __construct($username, $password, $host, $database) {
+        try {
+            $this->_dbHandle = new PDO("mysql:host=$host;dbname=$database",  $username, $password); // creates the database handle with connection info
+            //$this->_dbHandle = new PDO('mysql:host=' . $host . ';dbname=' . $database,  $username, $password); // creates the database handle with connection info
+
+        }
+        catch (PDOException $e) { // catch any failure to connect to the database
+            echo $e->getMessage();
+        }
+    }
+
+    /**
+     * @return PDO
+     */
+    public function getdbConnection() {
+        return $this->_dbHandle; // returns the PDO handle to be used                                        elsewhere
+    }
+
+    public function __destruct() {
+        $this->_dbHandle = null; // destroys the PDO handle when nolonger needed                                        longer needed
+    }
 }
--- Models/UserData.php
+++ Models/UserData.php
@@ -2,5 +2,54 @@
 
 class UserData
 {
+    protected $_userID, $_name, $_email, $_password, $_location, $_phoneNumber, $_profilePhoto, $_position;
+
+
+
+    public function __construct($dbRow) {
+        $this->_userID = $dbRow['userID'];
+        $this->_name = $dbRow['name'];
+        $this->_email = $dbRow['email'];
+        $this->_password = $dbRow['password'];
+        $this->_location = $dbRow['location'];
+        $this->_phoneNumber = $dbRow['phoneNumber'];
+        $this->_profilePhoto = $dbRow['profilePhoto'];
+        $this->_position = $dbRow['position'];
+
+    }
+
+    public function getUserID() {
+        return $this->_userID;
+    }
+
+    public function getName() {
+        return $this->_name;
+    }
+
+    public function getEmail() {
+        return $this->_email;
+    }
+
+    public function getPassword() {
+        return $this->_password;
+    }
+
+
+    public function getLocation() {
+        return $this->_location;
+    }
+
+    public function getPhoneNumber() {
+        return $this->_phoneNumber;
+    }
+
+    public function getProfilePhoto() {
+        return $this->_profilePhoto;
+    }
+
+    public function getPosition() {
+        return $this->_position;
+    }
+
 
 }
