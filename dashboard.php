69fbaea6cb3bacbfb32dc20d39101d177719952c
AGE851 <chiyinli.mastercyber1@gmail.com>
2022-12-08 13:46:34 +0000

    changed navbar align to right and font color to dark and underline

--- dashboard.php
+++ dashboard.php
@@ -3,12 +3,4 @@
 $view= new stdClass();
 $view->pageTitle = "Dashboard";
 
-$userInstance = new User();
-
-if(isset($_POST["LOGOUT"]))
-{
-    $userInstance->logout();
-}
-
-
 require_once('Views/dashboard.phtml');
