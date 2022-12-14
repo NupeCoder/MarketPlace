69fe84765745e20737d59329c7f793d61ffc1c3e
Fathi Mokhtar <70948032+falafel-cmd@users.noreply.github.com>
2022-12-14 01:09:39 +0000

    implemented some XSS protection

--- editlisting.php
+++ editlisting.php
@@ -3,12 +3,13 @@
 require_once('Models/User.php');
 require_once('Models/ListingsAPI.php');
 
-$view= new stdClass();
-$view->pageTitle = "Dashboard";
+$view = new stdClass();
+$view->pageTitle = "View Profile";
 
 $userInstance = new User();
 $listingAPI = new ListingsAPI();
 $editObj = $listingAPI->fetchCurrentListing();
+$phpSelf = filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_URL);
 
 if(isset($_POST['EditID']))
 {
@@ -17,7 +18,7 @@ if(isset($_POST['EditID']))
 
 if(isset($_POST['EDIT']))
 {
+
     $listingAPI->editListing($_POST['editName'], $_POST['editDesc'], $_POST['editPrice'], $_POST['editCategory']);
 }
-
 require_once ('Views/editlisting.phtml');
