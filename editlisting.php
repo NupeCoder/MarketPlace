c3b2045ab9d190535a04106cfe41cbc023d56c90
Fathi Mokhtar <70948032+falafel-cmd@users.noreply.github.com>
2022-12-14 01:46:35 +0000

    corrected mistakes regarding over-use of SESSION keys -> converted to POST key calls that insert into relevant method headers;

--- editlisting.php
+++ editlisting.php
@@ -8,17 +8,13 @@ $view->pageTitle = "View Profile";
 
 $userInstance = new User();
 $listingAPI = new ListingsAPI();
-$editObj = $listingAPI->fetchCurrentListing();
+$editObj = $listingAPI->fetchCurrentListing($_POST['EditID']);
 $phpSelf = filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_URL);
-
-if(isset($_POST['EditID']))
-{
-    $_SESSION['EditID'] = $_POST['EditID'];
-}
 
 if(isset($_POST['EDIT']))
 {
 
     $listingAPI->editListing($_POST['editName'], $_POST['editDesc'], $_POST['editPrice'], $_POST['editCategory']);
 }
+
 require_once ('Views/editlisting.phtml');
--- myprofile.php
+++ myprofile.php
@@ -1,11 +1,15 @@
 <?php
 
-
 require_once('Models/User.php');
 require_once('Models/UserAPI.php');
 
 $currentUser = new User(); // sets session id
 $listingAPI = new ListingsAPI();
+
+if(isset($_POST['DeleteID']))
+{
+    $listingAPI->removeChosenListing($_POST['DeleteID']);
+}
 
 require_once('Views/myprofile.phtml');
 
