58b5269e96699ddc381abc6914316f0261d3839e
Abubakar Liman <limanzaffar@gmail.com>
2022-12-15 17:09:37 +0000

    Changes on ListingsAPI.php and teamshook

--- Models/ListingsAPI.php
+++ Models/ListingsAPI.php
@@ -235,10 +235,10 @@ class ListingsAPI {
      */
     public function createCards(array $input): void
     {
-        $id = 0;
+       //$id = 0;
         foreach ($input as $listing)
         {
-            $id +=1;
+            //$id +=1;
             echo <<< EOT
                   <div class="col d-inline-flex justify-content-center g-4">
                         <div class="card border-red mb-3 me-3" style="width: 18rem;">
@@ -246,16 +246,15 @@ class ListingsAPI {
                                 <img src="{$listing->getItemPhoto()}" class="card-img-top" alt="Product Image" >
                             </div>
                             <div class="card-body text-center">
-                                <form method="get" action="">
-                                    <input type="hidden" class="card-title" name="name{$id}" id=name{$id} value="{$listing->getListingName()}">
+                                    <h6 class="card-title">{$listing->getListingName()}</h6>
                                     <h6 class="card-text text-muted">{$listing->getCategory()}</h6>
-                                    <h5 class="card-text" id="price{$id}">£{$listing->getPrice()}</h5>
+                                    <h5 class="card-text">£{$listing->getPrice()}</h5>
                                     <p class="card-text">{$listing->getDescription()}</p>
                                     <p class="card-footer">Seller: {$listing->getName()}</p>
                                     <img src="{$listing->getProfilePhoto()}"  class="card-img-profile" alt="Seller Profile Picture">
-                                    <button name={$id} type="submit" class="btn btn-outline-danger" >Send a Message</button>
+                                <form method="post" action="">
+                                    <button class="btn btn-outline-danger" name="teamsID" value="{$listing->getListingName()}, {$listing->getDescription()}" type="submit">Send a Message</button>
                                 </form>
-                                
                             </div>
                         </div>
                    </div>
@@ -266,4 +265,4 @@ class ListingsAPI {
 }
 
 
-
+//<input type="hidden" class="card-title" name="name{$id}" value="{$listing->getListingName()}">
--- Models/teamshook.php
+++ Models/teamshook.php
@@ -9,12 +9,14 @@ require('vendor\sebbmeyer\php-microsoft-teams-connector\src\Cards\SimpleCard.php
 $teamsurl = 'https://testlivesalfordac.webhook.office.com/webhookb2/239f714c-5d12-48b6-99b7-32c7bab59db1@65b52940-f4b6-41bd-833d-3033ecbcf6e1/IncomingWebhook/86c77cc816f54326b645352852d9da22/b5c3ae56-20df-476e-a296-eb50c262b730';
 
 
-if (isset($_GET['1']))
+if (isset($_POST['teamsID']))
 {
+
+    $listingInfo = explode(", ", $_POST['teamsID']);
 
     $connector = new \Sebbmyr\Teams\TeamsConnector($teamsurl);
 
-    $card  = new \Sebbmyr\Teams\Cards\SimpleCard(['title' => $_GET['name1'], 'text' => 'Testing']);
+    $card  = new \Sebbmyr\Teams\Cards\SimpleCard(['title' => $listingInfo[0], 'text' => $listingInfo[1]]);
 
     $connector->send($card);
 
